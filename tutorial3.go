389d65c399ef4fd94762e9bb185000eaff979e9f
lejaaprianza <lejaprianza@gmail.com>
2021-09-12 10:00:19 +0700

    tutorial 3

---
+++ tutorial3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Angka = ", 10)
+	fmt.Println("Angka float = ", 12.5)
+	fmt.Println("Boolean True = ", true)
+	fmt.Println("Boolean False = ", false)
+	fmt.Println("String1 = ", "ini string")
+	fmt.Println("String2"[0])
+	fmt.Println(len("String3"))
+}
