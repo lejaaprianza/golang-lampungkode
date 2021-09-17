554003fd2d46711847232dc50d37adf57ca9f08c
lejaaprianza <lejaprianza@gmail.com>
2021-09-17 07:39:36 +0700

    tutorial 5

---
+++ tutorial5.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	//contoh salah 1
+	// var daerah1 string
+	// daerah1 = "Lampung"
+
+	//contoh salah 2
+	// var daerah2
+
+	//contoh salah 3
+	// daerah3 := "Lampung Kode"
+	// daerah3 := "Bandung"
+}
